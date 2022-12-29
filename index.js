07ed7a4a80693ac9f09d8587075e4add40e117d7
rpapadev <rafanicolasp@hotmail.com>
2022-12-29 10:38:36 -0300

    continuação

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//utilizar o express com ejs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+
+var tarefas = ['Estudar', 'Dormir', 'Jogar']
+
+app.get('/', (req,res) => {
+    res.render('index', {tarefasList: tarefas});
+})
+
+app.get('/deletar/:id',(req,res) => {
+    tarefas = tarefas.filter((val,index) =>{
+        if (index != req.params.id) {
+            return val;
+        }
+    })
+    res.render('index', {tarefasList: tarefas});
+})
 
 app.listen(5000, () => {
     console.log('Servidor iniciado');
